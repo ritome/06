1499a4aafac5736444c700867e21e1189ad39448
ritome <ritome0109@gmail.com>
2020-10-21 11:10:09 +0900

    1回目の課題提出

--- index.php
+++ index.php
@@ -1,7 +1,54 @@
 <?php
 
 // 以下にCarクラスを定義して、当プログラムを実行した時にエラーがでないようにして下さい。
+class Car
+{
+    private $name;
+    private $number;
+    private $color;
 
+    public function __construct($name,$number,$color)
+    {
+        $this->name = $name;
+        $this->number = $number;
+        $this->color = $color;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getNumber()
+    {
+        return $this->number;
+    }
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+    public function setNumber($number)
+    {
+        $this->number = $number;
+    }
+    public function setColor($color)
+    {
+        $this->color = $color;
+    }
+
+
+
+    public function infomation() 
+    {
+        echo '車の車種:' . $this->name . '車体番号:' . $this->number . 'カラー:' . $this->color . 'です。<br>';
+    }
+
+}
 
 
 
@@ -9,7 +56,7 @@
 // 以下を実行した時にエラーがでないようにして下さい。
 // Carクラスのインスタンスを生成
 // 引数 : 車名、車体番号、カラー
-$car1 = new Car('ランクル', 100, 'black');
+$car1 = new Car('ランクル',100,'black');
 
 // 車名(車種)を取得
 echo $car1->getName();
@@ -45,3 +92,4 @@ echo '<br>';
 // 車の情報表示を表示
 echo $car1->infomation();
 echo '<br>';
+
