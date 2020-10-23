e67094300c14b6d2fcf875348b1140f4a8b04606
ritome <ritome0109@gmail.com>
2020-10-23 11:57:48 +0900

    3回目の提出

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
 
 // 以下にCarクラスを定義して、当プログラムを実行した時にエラーがでないようにして下さい。
-class Car
+Class Car
 {
-    private $name;
-    private $number;
-    private $color;
+    protected $name;
+    protected $number;
+    protected $color;
 
-    public function __construct($name,$number,$color)
+    public function __construct($name, $number, $color)
     {
         $this->name = $name;
         $this->number = $number;
@@ -43,53 +43,65 @@ class Car
 
 
 
-    public function infomation() 
+    public function information() 
     {
-        echo '車の車種:' . $this->name . '車体番号:' . $this->number . 'カラー:' . $this->color . 'です。<br>';
+        echo '車の車種:' . $this->name . '車体番号:' . $this->number . 'カラー:' . $this->color . 'です。';
     }
 
 }
 
+// 以下にCarクラスを継承したTaxiクラスを記述して下さい
+Class Taxi extends Car 
+{
+    private $passenger;
 
+    public function lower($lower)
+    {
+        if ($this->passenger >= $lower) {
+            echo $lower . '人降車しました。';
+        }
+        else
+        {
+            echo '降車人数に誤りがあります。';
+        }
+        $this->passenger -= $lower;
+    }
 
+    public function pickUp($pickUp)
+    {
+        $this->passenger += $pickUp;
+        echo '乗車人数は' . $this->passenger . '人です。';
+    }
+    public function infomation()
+    {
+        echo '車の車種:' . $this->name . '、車体番号:' . $this->number . '、カラー:' . $this->color . '、';
+    }
+}
 
-// 以下を実行した時にエラーがでないようにして下さい。
+// 以下を実行した時にエラーがでないようにして下さい
 // Carクラスのインスタンスを生成
 // 引数 : 車名、車体番号、カラー
 $car1 = new Car('ランクル', 100, 'black');
 
-// 車名(車種)を取得
-echo $car1->getName();
+// 車の情報を表示
+echo $car1->information();
+// echo '<br>';
+echo '<hr>';
+
+// Taxiクラスのインスタンスを生成
+$taxi1 = new Taxi('クラウンのタクシー', 222, 'black');
+// タクシーの情報表示を表示
+echo $taxi1->information();
+// echo '<br>';
+
+// 3人乗車
+$taxi1->pickUp(3);
 echo '<br>';
 
-// 車体番号を取得
-echo $car1->getNumber();
+// 2人降車
+$taxi1->lower(2);
 echo '<br>';
 
-// カラーを取得
-echo $car1->getColor();
+// 一人降車
+$taxi1->lower(2);
 echo '<br>';
-
-// 車の情報表示を表示
-echo $car1->infomation();
-echo '<br>';
-
-// 車名(車種)を更新
-$car1->setName('アルファード');
-echo $car1->getName();
-echo '<br>';
-
-// 車体番号を取得
-$car1->setNumber(200);
-echo $car1->getNumber();
-echo '<br>';
-
-// カラーを取得
-$car1->setColor('red');
-echo $car1->getColor();
-echo '<br>';
-
-// 車の情報表示を表示
-echo $car1->infomation();
-echo '<br>';
-
